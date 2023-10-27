a45a24a0db45a8d63bd8bd1e4aec7b7bac8b7f3f
Berenice Barcenas Mancilla <berenice.barcenas.mancilla@gmail.com>
2023-10-27 15:21:33 -0600

    Creacion de rutas, controladores y funciones para retornar las bistas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LibraryController;
+use App\Http\Controllers\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Ruta principal
+Route::get('/', [LibraryController::class, 'index'])->name('home');
+
+// Ruta de registro
+Route::get('/register', [RegisterController::class, 'index'])->name('libros');
+
+// Ruta para guardar registro
+Route::post('/save', [RegisterController::class, 'save'])->name('guardar');
